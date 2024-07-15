f66d2b4c8eab07094ff9f0b338b853fcd11654f1
romnick <romnicksanjuan9@gmail.com>
2024-07-15 20:01:53 +0800

    ss

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,33 +1,73 @@
-import React from 'react'
+import React, { useState, useSyncExternalStore } from 'react'
 import '../css/Contact.css'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const serviceId = 'service_o3jlica'
+    const templateId = 'template_2a9i2tr'
+    const publicKey = 'qF0SPfgPtEeerxnOs'
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: 'nick.dev',
+      message: message,
+      reply_to: email
+    }
+    
+    emailjs
+      .send(serviceId, templateId, templateParams, {
+        publicKey: publicKey,
+      })
+      .then(
+        (response) => {
+          console.log('SUCCESS!',response.status, response.text);
+          setName('')
+          setEmail('')
+          setMessage('')
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  }
+
   return (
     <div>
-      <h2 style={{textAlign:'center',color:'white', margin:'10px', fontSize:"2rem"}}>Contact Me</h2>
-    <div className='contact-main'>
-      <div className='item-container'>
-        <div className='items'>
-          <input type="text" className='item' placeholder='Full Name'  />
+      <form  onSubmit={sendEmail}>
+        <h2 style={{
+          textAlign: 'center', color: 'white',
+          margin: '10px', fontSize: "2rem", marginBottom: '15px'
+        }}>Contact Me</h2>
+        <div className='contact-main'>
+
+          <div className='item-container'>
+            <div className='items'>
+              <input type="text" className='item' value={name} onChange={(e) => setName(e.target.value)} placeholder='Full Name' />
+            </div>
+
+            <div className='items'>
+              <input type="email" className='item' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email Address' />
+            </div>
+
+          </div>
         </div>
 
-        <div className='items'>
-          <input type="text" className='item' placeholder='Mobile Number'  />
+        <div className='text-area'>
+          <textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Input Message...'></textarea>
         </div>
-
-        <div className='items'>
-          <input type="text" className='item' placeholder='Email Address'  />
+        <div style={{width:'100%',display:'flex',justifyContent:'center'}}>
+        <button style={{alignItems:'center',display:'flex',justifyContent:'center',height:'40px',
+           width:'30%',backgroundColor:'orange',color:'white',borderRadius:'5px'}} type='submit'>Send</button>
         </div>
-
-        <div className='items'>
-          <input type="email" className='item' placeholder='Email Subject'  />
-        </div>
-      </div>
-    </div>
-    
-      <div className='text-area'>
-        <textarea name="" id="" placeholder='Input Message...'></textarea>
-      </div>
+      </form>
     </div>
   )
 }
